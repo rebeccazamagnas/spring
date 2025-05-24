e1acf4400c897c131f4def69a7d864083d44714f
rebeccazamagnas <rebeccasix@rocketmail.com>
2025-05-24 12:32:36 -0300

    Primeiro commit

---
+++ src/main/java/com/example/demo2/entities/User.java
@@ -0,0 +1,68 @@
+package com.example.demo2.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+public class User implements Serializable {
+	
+	
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private String name;
+	private String email;
+	private String phone;
+	private String password;
+	
+	public User() {}
+
+	public User(Long id, String name, String email, String phone, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+
+}
