6aabde4b46efc65ddee313aab08d61a9bb916b23
rebeccazamagnas <rebeccasix@rocketmail.com>
2025-05-24 13:54:32 -0300

    Service Layer

--- src/main/java/com/example/demo2/repositories/UserRepository.java
+++ src/main/java/com/example/demo2/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo2.entities.User;
 
+
 public interface UserRepository  extends JpaRepository<User, Long>{
 	
 	
