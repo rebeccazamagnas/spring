e2b395aaec7ceee07dad61ac185d63f4834e7fa1
rebeccazamagnas <rebeccasix@rocketmail.com>
2025-05-24 13:30:19 -0300

    JPA repository

---
+++ src/main/java/com/example/demo2/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo2.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo2.entities.User;
+
+public interface UserRepository  extends JpaRepository<User, Long>{
+	
+	
+
+}
