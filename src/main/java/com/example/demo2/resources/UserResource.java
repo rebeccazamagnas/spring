309fe174c1c30b478805672965a4bce564ee46e6
rebeccazamagnas <rebeccasix@rocketmail.com>
2025-06-07 15:20:38 -0300

    User insert

--- src/main/java/com/example/demo2/resources/UserResource.java
+++ src/main/java/com/example/demo2/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.example.demo2.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.demo2.entities.User;
 import com.example.demo2.services.UserService;
@@ -20,6 +24,7 @@ public class UserResource {
 	
 	@Autowired
 	private UserService service;
+	
 	@GetMapping
 	public ResponseEntity<List<User>> findAll(){
 		
@@ -34,6 +39,20 @@ public class UserResource {
 		User obj = service.findById(id);
 		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		
+		obj = service.insert(obj);
+		
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId())
+				.toUri();
+		
+		
+		return ResponseEntity.created(uri).body(obj);
 	}
 	
 
