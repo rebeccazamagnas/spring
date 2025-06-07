cec9c8f013447ab6142cabe31c5e16650a557473
rebeccazamagnas <rebeccasix@rocketmail.com>
2025-06-07 14:26:34 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/example/demo2/entities/Product.java
+++ src/main/java/com/example/demo2/entities/Product.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -34,6 +37,9 @@ public class Product  implements Serializable{
 	joinColumns = @JoinColumn(name = "product_id"),
 	inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
+	
+	@OneToMany( mappedBy = "id.product")
+	private Set<OrderItem> itens = new HashSet<>();
 	
 	
 	public Product() {}
@@ -103,6 +109,15 @@ public class Product  implements Serializable{
 		Img = img;
 	}
 
+	@JsonIgnore
+	public Set<Order> getOrders() {
+		
+		Set<Order> set = new HashSet<>();
+		for (OrderItem x : itens) {
+			set.add(x.getOrder());
+		}
+		return set;
+	}
 
 	@Override
 	public int hashCode() {
