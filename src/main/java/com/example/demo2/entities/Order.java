df877622600553e749e2b751a56d76c8f6618499
rebeccazamagnas <rebeccasix@rocketmail.com>
2025-06-07 15:01:41 -0300

    Subtotal & Total Methods

--- src/main/java/com/example/demo2/entities/Order.java
+++ src/main/java/com/example/demo2/entities/Order.java
@@ -97,6 +97,16 @@ public class Order implements Serializable {
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
+	
+	public Double getTotal() {
+		
+		double sum = 0.0;
+		for (OrderItem x : itens) {
+			sum += x.getSubTotal();
+		}
+		
+		return sum;
+	}
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
--- src/main/java/com/example/demo2/entities/OrderItem.java
+++ src/main/java/com/example/demo2/entities/OrderItem.java
@@ -69,6 +69,12 @@ public class OrderItem implements Serializable {
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+	
+	public Double getSubTotal() {
+		
+		return price * quantity;
+	}
+	
 
 	@Override
 	public int hashCode() {
